3ff25af2d6f71870b2fe9bddfe679af7e72d8613
Benjamin Dums <benjamin.dums@gmail.com>
2024-10-05 19:13:30 +0200

    project merge and cleanup

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -13,7 +13,7 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
-        SceneManager.LoadScene("BennysTestScene");
+        SceneManager.LoadScene("Level1");
     }
 
     public void Exit()
