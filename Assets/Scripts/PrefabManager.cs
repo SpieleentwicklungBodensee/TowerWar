2b3ae4c1d2bc03780e48be6e85c2cf09a358c5dc
Benjamin Dums <benjamin.dums@gmail.com>
2024-10-06 12:25:56 +0200

    death sound effect

--- Assets/Scripts/PrefabManager.cs
+++ Assets/Scripts/PrefabManager.cs
@@ -7,6 +7,7 @@ public class PrefabManager : MonoBehaviour
     public GameObject shootSound1;
     public GameObject hitSound1;
     public GameObject hitSound2;
+    public GameObject deathSound1;
 
     private void Awake()
     {
